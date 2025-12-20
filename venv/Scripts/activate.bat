34cdfc091c2e7f2c5c8c8cba0fa50385473ea397
sugaringring <meaneatn@gmail.com>
2025-12-20 22:21:00 +0700

    test

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=D:\CADT\Year3\Cryptography\GO code\server version\venv
+set VIRTUAL_ENV=D:\CADT\Year3\Cryptography\Final Version Github\Final-Project-Crypto\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
